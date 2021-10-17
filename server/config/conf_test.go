3d410970af67cbcb99af3e3ee64da0afcad9b55e
iffigues <iffigues@vivaldi.net>
2021-10-17 18:52:45 +0200

    hello

--- server/config/conf_test.go
+++ server/config/conf_test.go
@@ -1,20 +1,22 @@
 package config
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestHelloEmpty(t *testing.T) {
 	c := NewConf()
-	go serve()
+
 	err := c.NewConfType("test", false)
 	if err != nil {
 		t.Fatalf("err one")
 	}
+
 	err = c.NewConfType("test", false)
 	if err == nil {
 		t.Fatalf("error 2")
 	}
+
 	err = c.NewConfType("test", true)
 	if err != nil {
 		t.Fatal("error 3")
